dbdbfa403435422f69b123ccfb2e3d28bbbfe551
Ethan Holdahl <59591844+ethanholdahl@users.noreply.github.com>
2022-03-10 21:54:27 -0800

    Created Automatic Game Table Graphic

--- Test Script.R
+++ Test Script.R
@@ -1,11 +1,16 @@
+install.packages("tidyverse")
+install.packages("ggplot2")
 library(tidyverse)
+library(ggplot2)
 
-game = makegame(3, 3)
+S1 = 6
+S2 = 4
 
-game = array(c(0, 0, 0, 0, 0, 0, 0, 0), dim = c(2, 2, 2))
-game = array(c(5, -1, -10, 3, -5, 3, -6, 19, 2, -2, 9, 11, -5, -1, 14, -4, 8, 4),
-             dim = c(3, 3, 2))
-game = array(c(3, 10, 12, 4, 15, 17, 7, 6, 10, 4, 4, 8), dim = c(2, 3, 2))
+game = makegame(S1, S2)
+
+#game = array(c(0, 0, 0, 0, 0, 0, 0, 0), dim = c(2, 2, 2))
+#game = array(c(5, -1, -10, 3, -5, 3, -6, 19, 2, -2, 9, 11, -5, -1, 14, -4, 8, 4), dim = c(3, 3, 2))
+#game = array(c(3, 10, 12, 4, 15, 17, 7, 6, 10, 4, 4, 8), dim = c(2, 3, 2))
 
 IEDSresult = IEDS(game)
 
@@ -45,4 +50,30 @@ NE
 ContinuousNE = FindContinuous(NE)
 
 ContinuousNE
+
+
+
+#GAME TABLE GRAPHIC
+
+S1 = 5
+S2 = 3
+game = makegame(S1,S2)
+
+straty = c(.5-(1:S1), rep(.2, each = S2))
+stratx = c(rep(-.2, each = S1), (1:S2)-.5)
+stratnames = c(paste0("s",1:S1), paste0("s",1:S2))
+stratcolors = c(rep("red", each = S1), rep("blue", each = S2))
+
+payoffs = as.vector(game)
+payoffsy =  c(.25-rep(1:S1, times = S2), .75-rep(1:S1, times = S2))
+payoffsx = c(rep(1:S2, each = S1)-.75, rep(1:S2, each = S1)-.25)
+payoffcolors = rep(c("red","blue"), each = S1*S2)
+
+ggplot()+
+  geom_segment(aes(x = seq(0,S2), y = 0, xend = seq(0,S2), yend = -S1))+
+  geom_segment(aes(x = 0, y = seq(0,-S1), xend = S2, yend = seq(0,-S1)))+
+  annotate("text", x = stratx, y = straty, size = 10, label = stratnames, color = stratcolors)+
+  annotate("text", x = payoffsx, y = payoffsy, size = 10, label = payoffs, color = payoffcolors)+
+  theme_void()+
+  coord_cartesian(xlim =c(-.3, S2), ylim = c(-S1,.3))
 
