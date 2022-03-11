54a392ca40625429d882af160700680d84e56dca
Ethan Holdahl <59591844+ethanholdahl@users.noreply.github.com>
2022-03-10 23:15:36 -0800

    Added GenerateGame Function
    Creates and returns all information about a game.

--- Functions.R
+++ Functions.R
@@ -1,4 +1,4 @@
-makegame = function(S1, S2) {
+MakeGame = function(S1, S2) {
   #This function creates a 3-dimensional array meant to represent payoffs in a 2 player S1 by S2 normal form game.
   
   #Name strategies
@@ -327,4 +327,55 @@ FindContinuous = function(NE) {
   }
   #return list of Nash Equilibria
   return(NE)
+}
+
+GenerateGame = function(S1,S2){
+  #Takes number of Player 1 and Player 2 strategies as inputs
+  #Returns a normal form game with randomly generated payoffs, list of dominated strategies, best response analysis, list of pure NE, list of all NE (in Nx2 or 2xN games)
+  game = MakeGame(S1, S2)
+  
+  #game = array(c(0, 0, 0, 0, 0, 0, 0, 0), dim = c(2, 2, 2))
+  #game = array(c(5, -1, -10, 3, -5, 3, -6, 19, 2, -2, 9, 11, -5, -1, 14, -4, 8, 4), dim = c(3, 3, 2))
+  #game = array(c(3, 10, 12, 4, 15, 17, 7, 6, 10, 4, 4, 8), dim = c(2, 3, 2))
+  
+  IEDSresult = IEDS(game)
+  
+  #IEDS animation: color row/column bright red + green for dominated + dominant rows/columns. Then bright green fade back to normal and bright red fade to dark red.
+  
+  dominated = IEDSresult[[1]]
+  dominators = IEDSresult[[2]]
+  #game
+  #game[-dominated[[1]],-dominated[[2]], ]
+  
+  
+  BRAresult = BRA(game, dominated)
+  p1BRs = BRAresult[[1]]
+  p2BRs = BRAresult[[2]]
+  remain1 = BRAresult[[3]]
+  remain2 = BRAresult[[4]]
+  elim1 = BRAresult[[5]]
+  elim2 = BRAresult[[6]]
+  
+  NE = FindPureNE(game, p1BRs, p2BRs, remain2)
+  PureNE = NE
+  if (length(remain1) == 2 & length(remain2) == 2) {
+    MixedNE = Find2x2MixedNE(game, remain1, remain2)
+    MixedNE
+    NE = c(NE, list(MixedNE))
+  } else {
+    if (length(remain1) == 2 | length(remain2) == 2) {
+      MixedNE = FindNx2MixedNE(game, remain1, remain2, p1BRs, p2BRs)
+      NE = c(NE, MixedNE)
+    }
+  }
+  
+  NE = NE[which(!(sapply(NE, is.null)))]
+  
+  #NE
+  
+  ContinuousNE = FindContinuous(NE)
+  
+  #ContinuousNE
+  
+  return(list(game, dominated, dominators, p1BRs, p2BRs, remain1, remain2, PureNE, ContinuousNE))
 }
