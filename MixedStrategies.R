db4d5c456e41e90e757c46685f7b0d958fd688f0
Ethan Holdahl <eholdahl@uoregon.edu>
2022-09-25 22:31:53 -0700

    added some automation to selecting BR and BR path, Added Expected payoff graph. Need: Fix for mulitple BR.

--- MixedStrategies.R
+++ MixedStrategies.R
@@ -1,6 +1,7 @@
 game = MakeGame(2,2)
 tabledata = MakeTableData(2,2,game)
-
+game
+tabledata[[3]] = c("Up", "Down", "Left", "Right")
 
 ggplot()+
   geom_segment(aes(x = seq(0,2), y = 0, xend = seq(0,2), yend = -2))+
@@ -9,6 +10,13 @@ ggplot()+
   annotate("text", x = tabledata[[5]], y = tabledata[[6]], size = 10, label = tabledata[[7]], color = tabledata[[8]])+
   theme_void()+
   coord_cartesian(xlim =c(-.3, 2), ylim = c(-2,.3))
+
+
+#find BRs
+
+BRs = BRA(game, list(0,0))
+BR1 = c(BRs[[1]][[1]], BRs[[1]][[2]])
+BR2 = c(BRs[[2]][[1]], BRs[[2]][[2]])
 
 
 ggplot()+
@@ -20,21 +28,21 @@ ggplot()+
   coord_cartesian(xlim =c(-.3, 2), ylim = c(-2,.3)) +
   annotate("path",
            x=.25+.1*cos(seq(0,2*pi,length.out=100)),
-           y=-.75+.1*sin(seq(0,2*pi,length.out=100)),
+           y=-BR1[1]+.25+.1*sin(seq(0,2*pi,length.out=100)),
            size = 1.5,
            color = "red") + 
   annotate("path",
            x=1.25+.1*cos(seq(0,2*pi,length.out=100)),
-           y=-1.75+.1*sin(seq(0,2*pi,length.out=100)),
+           y=-BR1[2]+.25+.1*sin(seq(0,2*pi,length.out=100)),
            size = 1.5,
            color = "red") + 
   annotate("path",
-           x=.75+.1*cos(seq(0,2*pi,length.out=100)),
+           x=BR2[1]-.25+.1*cos(seq(0,2*pi,length.out=100)),
            y=-.25+.1*sin(seq(0,2*pi,length.out=100)),
            size = 1.5,
            color = "blue") + 
   annotate("path",
-           x=1.75+.1*cos(seq(0,2*pi,length.out=100)),
+           x=BR2[2]-.25+.1*cos(seq(0,2*pi,length.out=100)),
            y=-1.25+.1*sin(seq(0,2*pi,length.out=100)),
            size = 1.5,
            color = "blue")
@@ -53,21 +61,21 @@ ggplot()+
   coord_cartesian(xlim =c(-.5, 2), ylim = c(-2,.5))  +
   annotate("path",
            x=.25+.1*cos(seq(0,2*pi,length.out=100)),
-           y=-.75+.1*sin(seq(0,2*pi,length.out=100)),
+           y=-BR1[1]+.25+.1*sin(seq(0,2*pi,length.out=100)),
            size = 1.5,
            color = "red") + 
   annotate("path",
            x=1.25+.1*cos(seq(0,2*pi,length.out=100)),
-           y=-1.75+.1*sin(seq(0,2*pi,length.out=100)),
+           y=-BR1[2]+.25+.1*sin(seq(0,2*pi,length.out=100)),
            size = 1.5,
            color = "red") + 
   annotate("path",
-           x=.75+.1*cos(seq(0,2*pi,length.out=100)),
+           x=BR2[1]-.25+.1*cos(seq(0,2*pi,length.out=100)),
            y=-.25+.1*sin(seq(0,2*pi,length.out=100)),
            size = 1.5,
            color = "blue") + 
   annotate("path",
-           x=1.75+.1*cos(seq(0,2*pi,length.out=100)),
+           x=BR2[2]-.25+.1*cos(seq(0,2*pi,length.out=100)),
            y=-1.25+.1*sin(seq(0,2*pi,length.out=100)),
            size = 1.5,
            color = "blue")
@@ -76,11 +84,12 @@ ggplot()+
 tabledataexpand = tabledata
 tabledataexpand[[1]] = c(-.2, -.2, -.2, .5, 1.5, 2.5)
 tabledataexpand[[2]] = c(-.5, -1.5, -2.5, .2, .2, .2)
-tabledataexpand[[3]] = c("s1", "p*s1+(1-p)*s2", "s2", "s1", "q*s1+(1-p)*s2", "s2")
+tabledataexpand[[3]] = c("Up", "p*Up+(1-p)*Down", "Down", "Left", "q*Left+(1-p)*Right", "Right")
 tabledataexpand[[4]] = c("red", "red", "red", "blue", "blue", "blue")
 tabledataexpand[[5]] = c(0.25, 0.25, 2.25, 2.25, 0.75, 0.75, 2.75, 2.75)
 tabledataexpand[[6]] = c(-0.75, -2.75, -0.75, -2.75, -0.25, -2.25, -0.25, -2.25)
-
+BR1expand = (BR1-1)*2+1
+BR2expand = (BR2-1)*2+1
 
 ggplot()+
   geom_segment(aes(x = seq(0,3), y = 0, xend = seq(0,3), yend = -3))+
@@ -90,23 +99,23 @@ ggplot()+
   theme_void()+
   coord_cartesian(xlim =c(-.5, 3), ylim = c(-3,.5))  +
   annotate("path",
-           x = .25+.1*cos(seq(0,2*pi,length.out=100)),
-           y = -.75+.1*sin(seq(0,2*pi,length.out=100)),
+           x=.25+.1*cos(seq(0,2*pi,length.out=100)),
+           y=-BR1expand[1]+.25+.1*sin(seq(0,2*pi,length.out=100)),
            size = 1.5,
            color = "red") + 
   annotate("path",
-           x = 2.25+.1*cos(seq(0,2*pi,length.out=100)),
-           y = -2.75+.1*sin(seq(0,2*pi,length.out=100)),
+           x=2.25+.1*cos(seq(0,2*pi,length.out=100)),
+           y=-BR1expand[2]+.25+.1*sin(seq(0,2*pi,length.out=100)),
            size = 1.5,
            color = "red") + 
   annotate("path",
-           x = .75+.1*cos(seq(0,2*pi,length.out=100)),
-           y = -.25+.1*sin(seq(0,2*pi,length.out=100)),
+           x=BR2expand[1]-.25+.1*cos(seq(0,2*pi,length.out=100)),
+           y=-.25+.1*sin(seq(0,2*pi,length.out=100)),
            size = 1.5,
            color = "blue") + 
   annotate("path",
-           x = 2.75+.1*cos(seq(0,2*pi,length.out=100)),
-           y = -2.25+.1*sin(seq(0,2*pi,length.out=100)),
+           x=BR2expand[2]-.25+.1*cos(seq(0,2*pi,length.out=100)),
+           y=-2.25+.1*sin(seq(0,2*pi,length.out=100)),
            size = 1.5,
            color = "blue") +
   # annotate("path",
@@ -121,11 +130,11 @@ ggplot()+
   #          size = 1.5)
   annotate("path",
            x = c(.5, 1.5, 1.5, 2.5),
-           y = c(-.5, -.5, -2.5, -2.5),
+           y = c(-BR1expand[1]+.5, -BR1expand[1]+.5, -BR1expand[2]+.5, -BR1expand[2]+.5),
            color = "red",
            size = 1.5) +
   annotate("path",
-           x = c(.5, .5, 2.5, 2.5),
+           x = c(BR2expand[1]-.5, BR2expand[1]-.5, BR2expand[2]-.5, BR2expand[2]-.5),
            y = c(-.5, -1.5, -1.5, -2.5),
            color = "blue",
            size = 1.5)
@@ -139,23 +148,23 @@ ggplot()+
   theme_void()+
   coord_cartesian(xlim =c(-.5, 3), ylim = c(-3,.5))  +
   annotate("path",
-           x = .25+.1*cos(seq(0,2*pi,length.out=100)),
-           y = -.75+.1*sin(seq(0,2*pi,length.out=100)),
+           x=.25+.1*cos(seq(0,2*pi,length.out=100)),
+           y=-BR1expand[1]+.25+.1*sin(seq(0,2*pi,length.out=100)),
            size = 1.5,
            color = "red") + 
   annotate("path",
-           x = 2.25+.1*cos(seq(0,2*pi,length.out=100)),
-           y = -2.75+.1*sin(seq(0,2*pi,length.out=100)),
+           x=2.25+.1*cos(seq(0,2*pi,length.out=100)),
+           y=-BR1expand[2]+.25+.1*sin(seq(0,2*pi,length.out=100)),
            size = 1.5,
            color = "red") + 
   annotate("path",
-           x = .75+.1*cos(seq(0,2*pi,length.out=100)),
-           y = -.25+.1*sin(seq(0,2*pi,length.out=100)),
+           x=BR2expand[1]-.25+.1*cos(seq(0,2*pi,length.out=100)),
+           y=-.25+.1*sin(seq(0,2*pi,length.out=100)),
            size = 1.5,
            color = "blue") + 
   annotate("path",
-           x = 2.75+.1*cos(seq(0,2*pi,length.out=100)),
-           y = -2.25+.1*sin(seq(0,2*pi,length.out=100)),
+           x=BR2expand[2]-.25+.1*cos(seq(0,2*pi,length.out=100)),
+           y=-2.25+.1*sin(seq(0,2*pi,length.out=100)),
            size = 1.5,
            color = "blue") +
   # annotate("path",
@@ -170,11 +179,11 @@ ggplot()+
   #          size = 1.5)
   annotate("path",
            x = c(.5, 1.5, 1.5, 2.5),
-           y = c(-.5, -.5, -2.5, -2.5),
+           y = c(-BR1expand[1]+.5, -BR1expand[1]+.5, -BR1expand[2]+.5, -BR1expand[2]+.5),
            color = "red",
            size = 1.5) +
   annotate("path",
-           x = c(.5, .5, 2.5, 2.5),
+           x = c(BR2expand[1]-.5, BR2expand[1]-.5, BR2expand[2]-.5, BR2expand[2]-.5),
            y = c(-.5, -1.5, -1.5, -2.5),
            color = "blue",
            size = 1.5) +
@@ -202,12 +211,134 @@ ggplot(data = BR, aes(x = q, y = p, color = Best_Response))+
   guides(size = "none") +
   annotate("text", x = .62, y = .23, size = 5, label = "Nash Equilibrium") +
   labs(x = "q", y = "p") +
-  geom_label(x = 0, y = 1, label = "(s1,s1)", show.legend = FALSE, color = "black") +
-  geom_label(x = 1, y = 1, label = "(s1,s2)", show.legend = FALSE, color = "black") +
-  geom_label(x = 0, y = 0, label = "(s2,s1)", show.legend = FALSE, color = "black") +
-  geom_label(x = 1, y = 0, label = "(s2,s2)", show.legend = FALSE, color = "black") +
+  geom_label(x = 0, y = 1, label = "(Up,Left)", show.legend = FALSE, color = "black") +
+  geom_label(x = 1, y = 1, label = "(Up,Right)", show.legend = FALSE, color = "black") +
+  geom_label(x = 0, y = 0, label = "(Down,Left)", show.legend = FALSE, color = "black") +
+  geom_label(x = 1, y = 0, label = "(Down,Right)", show.legend = FALSE, color = "black") +
   theme(text = element_text(size = 20),
         axis.title.x = element_text(colour = "blue"),
         axis.title.y = element_text(colour = "red"))
 
+
+ggplot()+
+  geom_segment(aes(x = seq(0,2), y = 0, xend = seq(0,2), yend = -2))+
+  geom_segment(aes(x = 0, y = seq(0,-2), xend = 2, yend = seq(0,-2)))+
+  annotate("text", x = tabledata[[1]], y = tabledata[[2]], size = 10, label = tabledata[[3]], color = tabledata[[4]])+
+  annotate("text", x = tabledata[[5]], y = tabledata[[6]], size = 10, label = tabledata[[7]], color = tabledata[[8]])+
+  theme_void()+
+  coord_cartesian(xlim =c(-.3, 2), ylim = c(-2,.3)) +
+  annotate("path",
+           x=.25+.1*cos(seq(0,2*pi,length.out=100)),
+           y=-BR1[1]+.25+.1*sin(seq(0,2*pi,length.out=100)),
+           size = 1.5,
+           color = "red") + 
+  annotate("path",
+           x=1.25+.1*cos(seq(0,2*pi,length.out=100)),
+           y=-BR1[2]+.25+.1*sin(seq(0,2*pi,length.out=100)),
+           size = 1.5,
+           color = "red") + 
+  annotate("path",
+           x=BR2[1]-.25+.1*cos(seq(0,2*pi,length.out=100)),
+           y=-.25+.1*sin(seq(0,2*pi,length.out=100)),
+           size = 1.5,
+           color = "blue") + 
+  annotate("path",
+           x=BR2[2]-.25+.1*cos(seq(0,2*pi,length.out=100)),
+           y=-1.25+.1*sin(seq(0,2*pi,length.out=100)),
+           size = 1.5,
+           color = "blue") +
+  annotate("path",
+           x = c(.5, 1, 1, 1.5),
+           y = c(-BR1[1]+.5, -BR1[1]+.5, -BR1[2]+.5, -BR1[2]+.5),
+           color = "red",
+           size = 1.5) +
+  annotate("path",
+           x = c(BR2[1]-.5, BR2[1]-.5, BR2[2]-.5, BR2[2]-.5),
+           y = c(-.5, -1, -1, -1.5),
+           color = "blue",
+           size = 1.5)
+
+ggplot()+
+  geom_segment(aes(x = seq(0,2), y = 0, xend = seq(0,2), yend = -2))+
+  geom_segment(aes(x = 0, y = seq(0,-2), xend = 2, yend = seq(0,-2)))+
+  annotate("text", x = tabledata[[1]], y = tabledata[[2]], size = 10, label = tabledata[[3]], color = tabledata[[4]])+
+  annotate("text", x = tabledata[[5]], y = tabledata[[6]], size = 10, label = tabledata[[7]], color = tabledata[[8]])+
+  theme_void()+
+  coord_cartesian(xlim =c(-.3, 2), ylim = c(-2,.3)) +
+  annotate("path",
+           x=.25+.1*cos(seq(0,2*pi,length.out=100)),
+           y=-BR1[1]+.25+.1*sin(seq(0,2*pi,length.out=100)),
+           size = 1.5,
+           color = "red") + 
+  annotate("path",
+           x=1.25+.1*cos(seq(0,2*pi,length.out=100)),
+           y=-BR1[2]+.25+.1*sin(seq(0,2*pi,length.out=100)),
+           size = 1.5,
+           color = "red") + 
+  annotate("path",
+           x=BR2[1]-.25+.1*cos(seq(0,2*pi,length.out=100)),
+           y=-.25+.1*sin(seq(0,2*pi,length.out=100)),
+           size = 1.5,
+           color = "blue") + 
+  annotate("path",
+           x=BR2[2]-.25+.1*cos(seq(0,2*pi,length.out=100)),
+           y=-1.25+.1*sin(seq(0,2*pi,length.out=100)),
+           size = 1.5,
+           color = "blue") +
+  annotate("path",
+           x = c(.5, 1, 1, 1.5),
+           y = c(-BR1[1]+.5, -BR1[1]+.5, -BR1[2]+.5, -BR1[2]+.5),
+           color = "red",
+           size = 1.5) +
+  annotate("path",
+           x = c(BR2[1]-.5, BR2[1]-.5, BR2[2]-.5, BR2[2]-.5),
+           y = c(-.5, -1, -1, -1.5),
+           color = "blue",
+           size = 1.5)  +
+  annotate("point",
+           x = c(.5, 1, 1.5),
+           y = c(-.5, -1, -1.5),
+           color = "black",
+           size = 5) +
+  annotate("text",
+           x = c(.5, 1, 1.5),
+           y = c(-.5, -1, -1.5),
+           label = "Nash Equilibrium",
+           color = "black",
+           hjust = 1.1,
+           vjust = -1,
+           size = 4)
+
+#SOURCE: https://www.economicsnetwork.ac.uk/iree/v7n2/garrett.pdf
+
+Payoffs1 = tibble(Strat = c(rep("Up", 2), rep("Down", 2)), 
+                ExpectedPayoff = as.vector(c(game[1,,1], game[2,,1])), 
+                q = c(0,1,0,1))
+Payoffs1$Strat <- factor(Payoffs1$Strat, levels = c("Up", "Down"))
+
+
+ggplot(data = Payoffs1, aes(x = q, y = ExpectedPayoff, color = Strat))+
+  geom_path(size = 1.5)+
+  theme(text = element_text(size = 20))+
+  scale_color_manual(name = "Strategy", values = c(rgb(1,.5,0), rgb(0,.5,0)))+
+  geom_point(aes(x = 3/5, y = 6, size = 1.5), color = "black", stroke = 3)+
+  guides(size = "none")+ 
+  ggtitle("Expected Payoffs for Player 1's Strategies given Player 2's Choice of q")+
+  ylab("Expected Payoff")
+
+
+Payoffs2 = tibble(Strat = c(rep("Left", 2), rep("Right", 2)), 
+                  ExpectedPayoff = as.vector(game[,,2]), 
+                  p = c(1,0,1,0))
+Payoffs2$Strat <- factor(Payoffs2$Strat, levels = c("Left", "Right"))
+
+
+ggplot(data = Payoffs2, aes(x = p, y = ExpectedPayoff, color = Strat))+
+  geom_path(size = 1.5)+
+  theme(text = element_text(size = 20))+
+  scale_color_manual(name = "Strategy", values = c(rgb(1,.5,0), rgb(0,.5,0)))+
+  geom_point(aes(x = 3/5, y = 6, size = 1.5), color = "black", stroke = 3)+
+  guides(size = "none")+ 
+  ggtitle("Expected Payoffs for Player 2's Strategies given Player 1's Choice of p")+
+  ylab("Expected Payoff")
 
